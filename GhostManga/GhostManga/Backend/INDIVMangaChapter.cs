1522939a5fc4d6db897b724405ed07d5d13ed3ec
PlasAJulian <julian_aplas@outlook.com>
2022-05-19 10:28:30 -0400

    some base added

---
+++ GhostManga/GhostManga/Backend/INDIVMangaChapter.cs
@@ -0,0 +1,15 @@
+﻿//This is what does into each individual chapter.
+using System;
+
+namespace GhostManga.Backend
+{
+    [Serializable]
+    class INDIVMangaChapter
+    {
+        //the title of the chapter
+        public string mangaChapterName { get; set; }
+        //Each chapter is linked to a URL that has all the pages/images of the chapter.
+        //this might be changed.
+        public string mangaChapterLink { get; set; }
+    }
+}
---
+++ GhostManga/GhostManga/Backend/INDIVMangaDetails.cs
@@ -0,0 +1,28 @@
+﻿//This is what does into each individual manga and all its details.
+using System;
+
+namespace GhostManga.Backend
+{
+    [Serializable]
+    class INDIVMangaDetails
+    {
+        public string mangaID { get; set; }
+        public string mangaName { get; set; }
+
+        //Since it is possable for a manga to have many names the API displays all the names as a list.
+        //might not be necessary until support for multiple languages
+        public string[] mangaAltNames { get; set; }
+        public string mangaAuthor { get; set; }
+
+        //Since it is possable for a manga to have several genre the API displays all the genres as a list.
+        public string[] mangaGenre { get; set; }
+        public string mangaRateScore { get; set; }
+        public string mangaStatus { get; set; }
+        public string mangaViews { get; set; }
+        public string mangaChapterNumber { get; set; }
+        public string mangaDescription { get; set; }
+        public string mangaImageLink { get; set; }
+        //each chatper is its own object so a list is made to hold all the chapters and its data.
+        public INDIVMangaChapter[] chapters { get; set; }
+    }
+}
