6b79e09734eef9e75c36f25613e4726227e8a17f
PlasAJulian <julian_aplas@outlook.com>
2022-05-25 10:44:27 -0400

    App icon and color theme

--- GhostManga/GhostManga.Android/MainActivity.cs
+++ GhostManga/GhostManga.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace GhostManga.Droid
 {
-    [Activity(Label = "GhostManga", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "GhostManga", Icon = "@mipmap/GhostMangaIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
