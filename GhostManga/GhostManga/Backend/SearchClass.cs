7a29a30b74ae8a4de1e6c1319dd50e92ea3391c7
PlasAJulian <julian_aplas@outlook.com>
2022-05-27 15:48:26 -0400

    api working with input from main page

---
+++ GhostManga/GhostManga/Backend/SearchClass.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace GhostManga.Backend
+{
+    class SearchClass
+    {
+        public SearchedMangeList SB = new SearchedMangeList();
+        public void GetSearchList(string sWord, string sType, string page)
+        {
+            SB = mainsearch(sWord, sType, page);
+        }
+        //takes the input to create the API URL that will access a json file of the what was found in the search.
+        static SearchedMangeList mainsearch(string sWord, string sType, string page)
+        {
+            //APIkeys will need to maed on your own
+            APIkeys a = new APIkeys();
+           
+            //opens the json with the searched items
+            WebClient c = new WebClient();
+            string searchJson = c.DownloadString(a.SearchKey+ "type=" + sType + "&search=" + sWord + "&page=" + page);
+            var search = JsonConvert.DeserializeObject<SearchedMangeList>(searchJson);
+            //returned everything in the json as a object
+            return search;
+        }
+    }
+}
--- GhostManga/GhostManga/MainPage.xaml.cs
+++ GhostManga/GhostManga/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GhostManga.Backend;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,19 +18,28 @@ namespace GhostManga
         }
         async void searchClick(object sender, EventArgs args)
         {
+            SearchClass s = new SearchClass();
+
             //display message when search word is empty
             if (searchWord.Text == null)
             {
-                await DisplayAlert("buttonClick","You didnt search anything", "ok");
+                await DisplayAlert("buttonClick", "You didnt search anything", "ok");
             }
             //if not type is selected title will be the default  
             else if (searchType.SelectedItem == null)
             {
-                await DisplayAlert("buttonClick", "Searchword= " + searchWord.Text + "\nSearchType= title" , "ok");
+                string word = searchWord.Text;
+                s.GetSearchList(word, "title", "1");
+                await DisplayAlert("Search made", "Looks like there are " + s.SB.numOfManga + " manga with horror in the title", "ok");
             }
             //everything that was typed and selected
             else
-                await DisplayAlert("buttonClick","Searchword= "+searchWord.Text+"\nSearchType= "+searchType.SelectedItem,"ok");
+            {
+                string word = searchWord.Text;
+                string typeword = searchType.SelectedItem.ToString();
+                s.GetSearchList(word, typeword, "1");
+                await DisplayAlert("Search made", "Looks like there are " + s.SB.numOfManga + " manga with horror in the title", "ok");
+            }
         }
     }
 }
